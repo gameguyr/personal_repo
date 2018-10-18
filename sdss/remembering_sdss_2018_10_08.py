e170a9082bd55cf636726ece18db423c95f4b6c5
russell.lego <russell.lego@tubemogul.com>
2018-10-18 11:17:08 -0700

    making a few files to remember how to interact with the FITS files

--- sdss/remembering_sdss_2018_10_08.py
+++ sdss/remembering_sdss_2018_10_08.py
@@ -12,29 +12,26 @@
 #################################
 import sdss_common
 import astropy.io.fits as pyfits
+import matplotlib.pyplot as plt
+import numpy as np
 #################################
 # Defining Constants
 #################################
-file_path_for_catalog = 'sdss_2016_01_13.fit'
-
-#################################
-# Defining Functions
-#################################
+# file_path_for_catalog = 'sdss_2016_01_13.fit'
+file_path_for_catalog = '/Users/lego/Dropbox/Research/thesis/Working Thesis 2018-01-02/astronomy_data/sdss_2016_01_13.fit'
 
 
-#################################
-# Defining Classes
-#################################
-
-
-#################################
-# Performing Work
-#################################
-
-# catalog_data = sdss_common.load_fits_file(file_path_for_catalog)
+catalog_data = sdss_common.load_fits_file(file_path_for_catalog)
 #
 # print catalog_data[1]
 
 # pyfits.info(file_path_for_catalog)
 catalog_header = pyfits.getheader(file_path_for_catalog)
 print catalog_header
+
+
+# TODO Need to plot number of objects vs decreasing magnitude
+# In order to do that, I have learn to index the catalog_data using pyfits.
+
+
+
---
+++ sdss/remembering_sdss_2018_10_17_PYFITS_REFERENCE.py
@@ -0,0 +1,62 @@
+#!/usr/bin/python
+########################
+# TITLE: remembering_sdss_2018_10_17
+# AUTHOR: russell lego
+# DATE: 2018-10-17
+# PURPOSE: trying to manipulate the fits file using the examples on the website.
+# http://docs.astropy.org/en/stable/io/fits/#f1
+#
+# The goal of this is to plot the number of objects as I decrease the magnitude.
+# to show that the number of objects is highly dependent on the magnitude
+########################
+
+
+from astropy.io import fits
+import numpy
+import matplotlib.pyplot as plt
+import numpy as np
+
+
+
+
+fits_image_filename = '/Users/lego/Dropbox/Research/thesis/Working Thesis 2018-01-02/astronomy_data/sdss_2016_01_13.fit'
+
+header_data_unit_list = fits.open(fits_image_filename)
+header_data_unit_table = header_data_unit_list[1]
+header_data_unit_table_data = header_data_unit_table.data
+
+# here's how to get all the columns of the data table
+columns_list = header_data_unit_table.columns.names
+
+# header_data_unit_list.info()
+
+
+# now getting the arrays to plot
+
+number_of_objects = []
+
+
+modelMag_u_max = max(header_data_unit_table_data['modelMag_u'])
+modelMag_u_min = min(header_data_unit_table_data['modelMag_u'])
+
+interval_of_magnitude = numpy.arange(modelMag_u_min, modelMag_u_max, 0.1)
+
+
+
+
+
+for i in range(0, len(interval_of_magnitude)):
+    index_where_modelMag_u_is_below = numpy.where(header_data_unit_table_data['modelMag_u'] < interval_of_magnitude[i])
+    number_of_objects.append(len(header_data_unit_table_data['modelMag_u'][index_where_modelMag_u_is_below]))
+
+# now I am going to plot the array of data that I have created.
+
+# plt.scatter(interval_of_magnitude, number_of_objects,  marker='o', s=0.3)
+# plt.xlabel('interval_of_magnitude')
+# plt.ylabel('number_of_objects')
+# plt.title('number_of_objects vs. interval_of_magnitude')
+# plt.show()
+
+
+
+histograms_values, bins = numpy.histogram(a, bins = [0,20,40,60,80,100])
